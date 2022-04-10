d2b786236bd9de29fb5489e1803e781c73a52a05
1834423612 <1834423612@qq.com>
2022-04-10 14:09:25 +0800

    规范图片后缀为.png

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php include_once 'count.php';?>
 <?php
-$domain = './theme/moebooru'; //主机名
-$file_path = 'counter.dat'; //访客数量存储文件位置
-$length = 7; //最大单位为百万 即7位数
-$count = trim(file_get_contents($file_path)); //读取文件中的访客数量数字 ，file_path 为访客数量存储文件
-$complete = str_repeat('0' , $length - strlen($count)) . $count; //自动补全为有0或没有0的7位数
-$imageArray = str_split($complete); //将字符串分割为字符数组
+$domain = './theme/moebooru'; // 文件目录名
+$file_path = 'counter.dat'; // 访问次数存储文件名
+$length = 7; // 最大单位为百万 即7位数。可修改
+$count = trim(file_get_contents($file_path)); // 读取访问次数
+$complete = str_repeat('0' , $length - strlen($count)) . $count; // 自动补全
+$imageArray = str_split($complete); // 将字符串分割为字符数组
 //$theme  = '';
 foreach($imageArray as $key => $value){
-	$imageArray[$key] = $domain . '/' . $value . '.gif'; //将数量对应为图片
-	echo "<img src='".$domain."/".$value.".gif' />";
+	$imageArray[$key] = $domain . '/' . $value . '.png'; // 将数量对应为图片
+	echo "<img src='".$domain."/".$value.".png' />";
 }
 
 //print_r($imageArray);
